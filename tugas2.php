cd145fed3a7ffc5c87581717fa6e31fd48f29279
putrihndyani <putrihndyani253@gmail.com>
2024-03-28 07:38:53 +0700

    terbaru

--- tugas2.php
+++ tugas2.php
@@ -4,103 +4,161 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form</title>
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 </head>
 
-<body align="center">
+<body>
+
+
+    <?php
+    $nama = $_POST["nama"];
+    $produk = $_POST["produk"];
+    $jumlahBeli = $_POST["jumlahBeli"];
+
+    if ($produk == "Tv" && $jumlahBeli > 1) {
+        $jumlahBeli;
+        $hargaSatuan = 2250000;
+        $totalBelanja = $hargaSatuan;
+        $diskon = 0 * $totalBelanja;
+        $ppn = 0.1 * ($totalBelanja - $diskon);
+        $hargaBersih = ($totalBelanja - $diskon) + $ppn;
+
+        if ($jumlahBeli > 1) {
+            $totalBelanja = $jumlahBeli * $hargaSatuan;
+            $diskon = 0.2 * $totalBelanja;
+            $ppn = 0.1 * ($totalBelanja - $diskon);
+            $hargaBersih = ($totalBelanja - $diskon) + $ppn;
+        }
+    } else if ($produk == "Kulkas" && $jumlahBeli > 0) {
+        $jumlahBeli;
+        $hargaSatuan = 3000000;
+        $totalBelanja = $hargaSatuan;
+        $diskon = 0 * $totalBelanja;
+        $ppn = 0.1 * ($totalBelanja - $diskon);
+        $hargaBersih = ($totalBelanja - $diskon) + $ppn;
+
+        if ($jumlahBeli > 1) {
+            $totalBelanja = $jumlahBeli * $hargaSatuan;
+            $diskon = 0.2 * $totalBelanja;
+            $ppn = 0.1 * ($totalBelanja - $diskon);
+            $hargaBersih = ($totalBelanja - $diskon) + $ppn;
+        }
+    } else if ($produk == "MesinCuci" && $jumlahBeli > 0) {
+        $jumlahBeli;
+        $hargaSatuan = 12000000;
+        $totalBelanja = $hargaSatuan;
+        $diskon = 0 * $totalBelanja;
+        $ppn = 0.1 * ($totalBelanja - $diskon);
+        $hargaBersih = ($totalBelanja - $diskon) + $ppn;
+
+        if ($jumlahBeli > 1) {
+            $totalBelanja = $jumlahBeli * $hargaSatuan;
+            $diskon = 0.2 * $totalBelanja;
+            $ppn = 0.1 * ($totalBelanja - $diskon);
+            $hargaBersih = ($totalBelanja - $diskon) + $ppn;
+        }
+    } else if ($produk == "Ac" && $jumlahBeli > 0) {
+        $jumlahBeli;
+        $hargaSatuan = 4000000;
+        $totalBelanja = $hargaSatuan;
+        $diskon = 0 * $totalBelanja;
+        $ppn = 0.1 * ($totalBelanja - $diskon);
+        $hargaBersih = ($totalBelanja - $diskon) + $ppn;
+
+        if ($jumlahBeli > 1) {
+            $totalBelanja = $jumlahBeli * $hargaSatuan;
+            $diskon = 0.2 * $totalBelanja;
+            $ppn = 0.1 * ($totalBelanja - $diskon);
+            $hargaBersih = ($totalBelanja - $diskon) + $ppn;
+        }
+    } else {
+        if ($produk == "") {
+            $jumlahBeli = 0;
+            $hargaSatuan = 0;
+            $totalBelanja = $hargaSatuan;
+            $diskon = 0 * $totalBelanja;
+            $ppn = 0.1 * ($totalBelanja - $diskon);
+            $hargaBersih = ($totalBelanja - $diskon) + $ppn;
+        }
+    }
+
+    ?>
     <h1>Form Pembeli</h1>
-    <form action="#" method="POST">
-        <table border="1" align="center" cellpadding="15" width="40%">
-            <thead>
-                <tr bgcolor="plum">
-                    <th colspan="2">Form Pembeli</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td> <label for="nama" class="col-4 col-form-label">Nama Pelanggan:</label></td>
-                    <td> <input type="text" name="nama" id="" placeholder="Isi Form Ini" size="50"></td>
-                </tr>
-                <tr>
-                    <td><label for="produk" class="col-4 col-form-label">Produk Pilihan:</label></td>
-                    <td>
-                        <select id="produk" name="produk" class="custom-select">
-                            <option value="">---- Pilihan ----</option>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4">
+                <form action="" method="post" class="">
+
+                    <div class="form-group">
+                        <label for="" class="form-label">Nama Pelanggan</label>
+                        <input type="text" name="nama" id="" class="form-control" placeholder="inputkan nama anda">
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="form-label">Produk pilihan</label>
+                        <select name="produk" id="" class="custom-select">
+                            <option value="">---Produk Pilihan ---</option>
                             <option value="Tv">TV</option>
-                            <option value="Kulkas">KULKAS</option>
+                            <option value="Kulkas">Kulkas</option>
                             <option value="MesinCuci">MESIN CUCI</option>
                             <option value="Ac">AC</option>
                         </select>
-                    </td>
-                </tr>
-                <td>
-                    <label for="jumlahbeli" class="col-4 col-form-label">Jumlah Beli:</label>
-                </td>
-                <td> <input id="jumlahbeli" name="jumlahbeli" type="number" class="form-control"></td>
-                </tr>
-            </tbody>
-            <tfoot bgcolor="plum">
-                <tr>
-                    <th colspan="2">
-                        <div class="form-group row">
-                            <div class="offset-4 col-8">
-                                <button name="submit" type="submit" class="btn btn-primary">Submit</button>
-                                <button name="batal" type="batal" class="btn btn-success">Batal</button>
-                            </div>
-                        </div>
-                    </th>
-                </tr>
-            </tfoot>
-        </table>
-    </form>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="" class="form-label">jumlah Beli</label>
+                        <input type="number" name="jumlahBeli" id="" class="form-control" placeholder="inputkan jumlah Beli">
+                    </div>
+                    <button type="submit" name="proses" class="btn btn-primary">Submit</button>
+                    <button type="batal" name="proses" class="btn btn-success">Batal</button>
+                </form>
+            </div>
+            <?php if (isset($_POST["proses"])) { ?>
+                <div class="col-md-4">
+                    <h1>Hasil Data Pelanggan</h1><br>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr bgcolor="plum">
+                                <td>Nama</td>
+                                <td><?= $nama ?> </td>
+                            </tr>
+                            <tr>
+                                <td>Produk</td>
+                                <td><?= $produk ?> </td>
+                            </tr>
+                            <tr>
+                                <td>jumlahBeli</td>
+                                <td><?= $jumlahBeli ?> </td>
+                            </tr>
+                            <tr>
+                                <td>harga satuan</td>
+                                <td>Rp <?= number_format($hargaSatuan, 0, ',', '.') ?> </td>
+                            </tr>
+                            <tr>
+                                <td>total belanja</td>
+                                <td>Rp <?= number_format($totalBelanja, 0, ',', '.') ?></td>
+                            </tr>
+                            <tr>
+                                <td>diskon</td>
+                                <td>Rp <?= number_format($diskon, 0, ',', '.') ?></td>
+                            </tr>
+                            <tr>
+                                <td>ppn</td>
+                                <td>Rp <?= number_format($ppn, 0, ',', '.') ?></td>
+                            </tr>
+                            <tr>
+                                <td>harga bersih</td>
+                                <td>Rp <?= number_format($hargaBersih, 0, ',', '.') ?></td>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
-
-<?php
-$nama = $_POST['nama'];
-$produk = $_POST['produk'];
-$jumlahbeli = $_POST['jumlahbeli'];
-
-
-//SWITCH CASE
-switch ($produk) {
-    case "Tv":
-        $hargaSatuan = 1500000;
-        break;
-    case "Kulkas":
-        $hargaSatuan = 3000000;
-        break;
-    case "Mesincuci":
-        $hargaSatuan = 2000000;
-        break;
-    case "Ac":
-        $hargaSatuan = 4000000;
-        break;
-    default:
-        // Jika produk tidak dipilih atau tidak sesuai
-        $hargaSatuan = 0;
-}
-// Hitung total belanja
-$totalBelanja = $jumlahbeli * $hargaSatuan;
-
-// Hitung diskon (20% dari total belanja)
-$diskon = 0.2 * $totalBelanja;
-
-// Hitung PPN (10% dari total belanja setelah diskon)
-$ppn = 0.1 * ($totalBelanja - $diskon);
-
-// Hitung harga bersih
-$hargaBersih = ($totalBelanja - $diskon) + $ppn;
-?>
-
-<!-- Cetak di dalam css -->
-<h1 align="center">Informasi Pelanggan </h1>
-<h2>Nama Pelanggan :<?= $nama ?> </h2>
-<h2>Produk Pilihan :<?= $produk ?> </h2>
-<h2>Jumlah Beli :<?= $jumlahbeli ?> </h2>
-<h2>Harga Satuan :<?= $hargaSatuan ?> </h2>
-<h2>Total Belanja :<?= $totalBelanja  ?> </h2>
-<h2>Diskon :<?= $diskon ?> </h2>
-<h2>PPN :<?= $ppn ?> </h2>
-<h2>Harga Bersih :<?= $hargaBersih ?> </h2>
